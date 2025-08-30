991fd898328298a7fbbaa1ac157bffcc1ca852cc
Priya <155173659+preyaah@users.noreply.github.com>
2025-08-30 23:21:50 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://Priya:22122002@cluster0.4gllkd3.mongodb.net/tomato-food-delivery-website').then(()=>console.log("DB Connected"));
+    await mongoose.connect('secret').then(()=>console.log("DB Connected"));
 }
